a6342318d9684453a8591553b99acca861596d3d
Derek Hewitt <derekmhewitt@gmail.com>
2016-08-09 13:48:53 -0700

    first test working

---
+++ src/series.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""series file."""
+
+
+def fibonacci(n):
+    """Find nth value in Fibonacci sequence."""
+    fib_list = [0, 1]
+    return fib_list[n - 1]
---
+++ src/test_series.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""test_series file."""
+
+
+def test_fibonacci_1():
+    """test_fib_1 function."""
+    from series import fibonacci
+    assert fibonacci(1) == 0
+    # assert fibonacci(7) == 8
