0a766db9f20e22f99e2294375503dce2724278c1
Derek Hewitt <derekmhewitt@gmail.com>
2016-08-09 18:11:38 -0700

    More testing on Lucas, finished name = main block

--- src/series.py
+++ src/series.py
@@ -12,16 +12,50 @@ def fibonacci(n):
             fib_list.append(first + second)
         return fib_list[n - 1]
     elif n == 0:
-        return 0
+        return None
     else:
-        raise ValueError('Please input a positive integer.')
+        return None
 
 
 def lucas(n):
     """Find nth in Lucas sequence."""
-    if n <= 1:
+    if n == 0 or not isinstance(n, int):
+        return None
+    elif n == 1:
         return 2
     elif n == 2:
         return 1
     else:
         return lucas(n - 1) + lucas(n - 2)
+
+
+def sum_series(n, f_num=0, s_num=1):
+    """Find nth in a series defined by first and second."""
+    sum_list = [f_num, s_num]
+    if isinstance(n, int) and n > 0:
+        for iterations in range(n):
+            first = sum_list[len(sum_list) - 2]
+            second = sum_list[len(sum_list) - 1]
+            sum_list.append(first + second)
+        return sum_list[n - 1]
+    elif n == 0:
+        return None
+    else:
+        return None
+
+
+if __name__ == '__main__':
+    print(u"This module defines 3 functions that implement different mathematical series.")
+    print("fibonacci(n)")
+    print(u"The first function this module contains outputs the value of the nth Fibonacci number. This is the output of the Fibonacci function called with n = 7:")
+    print(u">>> fibonacci(7)")
+    print(fibonacci(7))
+    print(u"The second function this module contains outputs the value of the nth Lucas number. This is the output of the Lucas function called with n = 5:")
+    print(u">>> lucas(5)")
+    print(lucas(5))
+    print(u"The thrid and final function in this module outputs the value of the nth number in Fibonacci or Lucas -like sequence defined by the user with it's second and third arguments. This is the output of the sum_series function called with n = 3, a = 5 and b = 7:")
+    print(u">>> sum_series(3, 5, 7)")
+    print(sum_series(3, 5, 7))
+    print(u"You can also execute sum_series with just an n argument and it will default to a Fibonacci number output:")
+    print(u">>> sum_series(6)")
+    print(sum_series(6))
--- src/test_series.py
+++ src/test_series.py
@@ -3,7 +3,7 @@
 import pytest
 
 FIBONACCI_TABLE = [
-    (0, 0),
+    (0, None),
     (1, 0),
     (2, 1),
     (3, 1),
@@ -15,7 +15,7 @@ FIBONACCI_TABLE = [
 ]
 
 LUCAS_TABLE = [
-    (0, 0),
+    (0, None),
     (1, 2),
     (2, 1),
     (3, 3),
@@ -23,6 +23,13 @@ LUCAS_TABLE = [
     (5, 7),
     (10, 76),
     (11, 123)
+]
+
+SUM_SERIES_TABLE = [
+    (5, 7, 4, 26),
+    (4, 3, 4, 11),
+    (0, 12, 14, None),
+    (-5, 3, 4, None)
 ]
 
 
@@ -45,16 +52,16 @@ def test_fibonacci_param(n, result):
     assert fibonacci(n) == result
 
 
-# def test_fibonacci_not_int():
-#     """Test for not integer inputs."""
-#     from series import fibonacci
-#     assert fibonacci('taco') == ValueError
+def test_fibonacci_not_int():
+    """Test for not integer inputs."""
+    from series import fibonacci
+    assert fibonacci('taco') is None
 
 
-# def test_fibonacci_negatives():
-#     """Test for rejecting negative inputs."""
-#     from series import fibonacci
-#     assert fibonacci(-1) == u'ValueError: Please input a positive integer.'
+def test_fibonacci_negatives():
+    """Test for rejecting negative inputs."""
+    from series import fibonacci
+    assert fibonacci(-1) is None
 
 
 def test_lucas_1():
@@ -73,3 +80,23 @@ def test_lucas_7():
     """Test lucas with input 7."""
     from series import lucas
     assert lucas(7) == 18
+
+
+@pytest.mark.parametrize('n, result', LUCAS_TABLE)
+def test_lucas_params(n, result):
+    """Test lucas function using the LUCAS_TABLE."""
+    from series import lucas
+    assert lucas(n) == result
+
+
+def test_sum_series():
+    """Test sum_series function with n = 6 and first = 1, second = 3."""
+    from series import sum_series
+    assert sum_series(6, 1, 3) == 18
+
+
+@pytest.mark.parametrize('n, a, b, result', SUM_SERIES_TABLE)
+def test_sum_param(n, a, b, result):
+    """Test sum_series function against SUM_SERIES_TABLE."""
+    from series import sum_series
+    assert sum_series(n, a, b) == result
